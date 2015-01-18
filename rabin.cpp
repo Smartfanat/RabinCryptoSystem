8fbb1d202f8b2fea6e1919eee8be601e1f814358
Evgeniy Dundyk <Smartfanat@gmail.com>
2015-01-18 08:09:17 +0200

    Update rabin.cpp

--- rabin.cpp
+++ rabin.cpp
@@ -5,29 +5,8 @@
 #include <vector>
 #include <cstring>
 using namespace std;
-
 int p = 167, q = 151;
 int n = p * q;
-
-string dec2bin(int number)
-{
-    if ( number == 0 ) return "0";
-    if ( number == 1 ) return "1";
-
-    if ( number % 2 == 0 )
-        return dec2bin(number / 2) + "0";
-    else
-        return dec2bin(number / 2) + "1";
-}
-
-int bin2dec(string number)
-{
-    int result = 0, pow = 1;
-    for ( int i = number.length() - 1; i >= 0; --i, pow <<= 1 )
-        result += (number[i] - '0') * pow;
-
-    return result;
-}
 
 int modulo (int a, int b)
 {
@@ -54,8 +33,6 @@ void decode(vector<int> s)
 
 int encrypt(int m, int p, int q)
 {
-    string t = dec2bin(m);
-    m = bin2dec(t);
     int c = (m * m)%n;
     return c;
 }
@@ -72,10 +49,10 @@ int mod(int k, int b, int m)
     }
     for(int j=0; j<i; j++){
         if(t[j]==1){
-        a=(a*b)%m;
-        b=(b*b)%m;
-    } else{
-        b=(b*b)%m;
+            a=(a*b)%m;
+            b=(b*b)%m;
+        } else{
+            b=(b*b)%m;
         }
     }
     return a;
@@ -86,7 +63,6 @@ vector<int> eea(int a, int b)
     if (b>a) {
         int temp=a; a=b; b=temp;
     }
-
     int x=0;
     int y=1;
     int lastx=1;
@@ -96,11 +72,9 @@ vector<int> eea(int a, int b)
         int temp1= a%b;
         a=b;
         b=temp1;
-
         int temp2 = x;
         x=lastx-q*x;
         lastx = temp2;
-
         int temp3 = y;
         y = lasty-q*y;
         lasty=temp3;
@@ -117,23 +91,17 @@ int decrypt(int c, int p, int q)
     int mp = mod((p+1)/4, c, p);
     int mq = mod((q+1)/4, c, q);
     vector<int> arr = eea(p, q);
-
     int pp = arr[0]*p*mq;
     int qq = arr[1]*q*mp;
-
     double r = modulo((pp+qq), n);
-
     if( r < 128)
         return r;
     int negative_r = n - r;
-
     if (negative_r < 128)
         return negative_r;
-
     int s = modulo((pp-qq), n);
     if( s < 128)
         return s;
-
     int negative_s = n - s;
     if( negative_s < 128)
         return negative_s;
@@ -142,28 +110,22 @@ int decrypt(int c, int p, int q)
 int main()
 {
     string test ="Rabin Cryptosystem";
-
-    cout << "Message:         " << test << endl;
+    cout << "Message: " << test << endl;
     int len = strlen(test.c_str());
-
     vector<int>l;
     for(int i = 0; i <= len; i++)
     {
         l.push_back(encrypt(test[i], p, q));
     }
-    cout <<  "Encryption:      ";
+    cout << "Encryption: ";
     copy(l.begin(), l.end(), ostream_iterator<int>(cout, ""));
-
-
     vector<int>d;
     for(int i = 0; i < len; i++)
     {
         d.push_back(decrypt(l[i], p, q));
     }
-
     cout << "\nDecoded message: ";
     decode(d);
     cout << endl;
-
     return 0;
 }
